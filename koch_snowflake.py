95321f893c0dfb632701a0e3407fc6f6791abef4
walle <mohouali.748@gmail.com>
2024-07-22 07:02:15 +0200

    Create koch_snowflake.py

---
+++ koch_snowflake.py
@@ -0,0 +1,10 @@
+from turtle import *
+import turtle as ttle
+
+def drawSnowFlake( nbFlake , SideLength) :
+#  if nbFlake == 0:
+#    ttle.foward( SideLength)
+#  drawSnowFlake(nbFlake -1, SideLength - 1)
+  return 
+# todo : finish it
+  
